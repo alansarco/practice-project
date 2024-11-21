4819ff853ed12fcd2f7ce1f7486bb4a2365841cb
Alan Sarco <asarco@csyoutsourcing.com>
2024-11-21 10:54:36 +0800

    down server

--- frontend/src/components/Api/ApiRoutes.js
+++ frontend/src/components/Api/ApiRoutes.js
@@ -1,5 +1,5 @@
-// export const mainRoute = "http://127.0.0.1:8000";
-export const mainRoute = "https://seahorse-app-to578.ondigitalocean.app/app";
+export const mainRoute = "http://127.0.0.1:8000";
+// export const mainRoute = "https://seahorse-app-to578.ondigitalocean.app/app";
 
 export const apiRoutes = {  
     login: `${mainRoute}/api/login`,
@@ -83,6 +83,9 @@ export const apiRoutes = {
 
     retrieveSettings: `${mainRoute}/api/settings`,
     updateSettings: `${mainRoute}/api/settings/updatesettings`,
+
+    orgSelect: `${mainRoute}/api/organizations/orgselect`,
+
 
 
     
