ed27afed43393dceb240a9b2c879787822165400
Alan Sarco <asarco@csyoutsourcing.com>
2024-12-25 16:24:31 +0800

    last

--- frontend/src/components/Api/Gateway.js
+++ frontend/src/components/Api/Gateway.js
@@ -1,8 +1,8 @@
 
 export const passToSuccessLogs = (message, path) => {
-      // console.log('Success Response:', message, path);
+      console.log('Success Response:', message, path);
 };
 
 export const passToErrorLogs = (message, path) => {   
-      // console.log('Error Response:', message, path);
+      console.log('Error Response:', message, path);
 }
